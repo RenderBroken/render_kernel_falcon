63570b834da18c4c6839704a3b25104c7849bfda
faux123 <reioux@gmail.com>
2013-02-02 08:35:08 -0800

    kernel: Move REPEAT_BYTE definition into linux/kernel.h
    And make sure that everything using it explicitly includes
    that header file.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>
    modified for Mako kernel from kernel.org
    
    Signed-off-by: faux123 <reioux@gmail.com>
    
    Conflicts:
    	arch/x86/include/asm/word-at-a-time.h

--- arch/x86/include/asm/word-at-a-time.h
+++ arch/x86/include/asm/word-at-a-time.h
@@ -1,6 +1,8 @@
 #ifndef _ASM_WORD_AT_A_TIME_H
 #define _ASM_WORD_AT_A_TIME_H
 
+#include <linux/kernel.h>
+
 /*
  * This is largely generic for little-endian machines, but the
  * optimal byte mask counting is probably going to be something
