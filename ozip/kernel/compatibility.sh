324d7e869e3033cff19e1dcc49bfb5d2c00f5f04
renderbroken <zkennedy87@gmail.com>
2014-05-12 10:19:08 -0700

    Build: Add new SWAP Scripts and adjust build script for it

--- ozip/kernel/compatibility.sh
+++ ozip/kernel/compatibility.sh
@@ -8,18 +8,27 @@ rm -rf $INITD_DIR/01mpdecision
 
 # pink
 rm -rf $INITD_DIR/98pink
+
 rm -rf /system/etc/init.pink.post_boot.sh
 rm -rf /system/etc/thermal-engine-pink.conf
 rm -rf /system/addon.d/98-pink.sh
 rm -rf /init.pink.post_boot.sh
 
+# Remove Custom Swap Space (Thanks Delta-roh!!)
+rm -rf $INITD_DIR/98swap_cache_v300
+rm -rf $INITD_DIR/98swap_data_v300
+rm -rf $INITD_DIR/98swap_system_v300
+rm -rf /cache/swap/*
+rm -rf /data/swap/*
+rm -rf /system/swap/*
+
 # remove the binaries as they are no longer needed. (kernel handled)
 if [ -e /system/bin/mpdecision ] ; then
 	busybox mv /system/bin/mpdecision /system/bin/mpdecision_bck
 fi
-#if [ -e /system/bin/thermald ] ; then
-#	busybox mv /system/bin/thermald /system/bin/thermald_bck
-#fi
+if [ -e /system/bin/thermald ] ; then
+	busybox mv /system/bin/thermald /system/bin/thermald_bck
+fi
 
 # remove dalvik cache
 #rm -rf /data/dalvik-cache
