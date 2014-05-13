909802c7dcf707aafcbb561bfbf4573bb7a766b6
renderbroken <zkennedy87@gmail.com>
2014-05-12 19:36:37 -0700

    cpufreq: re-add 92MHz back to underclock

--- arch/arm/mach-msm/acpuclock-8226.c
+++ arch/arm/mach-msm/acpuclock-8226.c
@@ -61,6 +61,7 @@ static struct msm_bus_scale_pdata bus_client_pdata = {
 
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p1[] = {
 #ifdef CONFIG_CPU_UNDERCLOCK
+	{ 1,   96000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  249600, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 #endif
@@ -76,6 +77,7 @@ static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p1[] = {
 
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p2[] = {
 #ifdef CONFIG_CPU_UNDERCLOCK
+	{ 1,   96000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  249600, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 #endif
@@ -91,6 +93,7 @@ static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p2[] = {
 
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p4[] = {
 #ifdef CONFIG_CPU_UNDERCLOCK
+	{ 1,   96000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  249600, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 #endif
@@ -109,6 +112,7 @@ static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p4[] = {
 
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p6[] = {
 #ifdef CONFIG_CPU_UNDERCLOCK
+	{ 1,   96000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  249600, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 #endif
