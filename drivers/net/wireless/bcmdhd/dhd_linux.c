eec3567fc2681769aedb9cfb8366aa07cefc6379
Pranav Vashi <neobuddy89@gmail.com>
2014-05-14 18:34:38 +0530

    sched/rt: Perform cleanup for moved rt specific bits
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/cpufreq_intelliactive.c
    	drivers/net/wireless/bcmdhd/dhd_linux.c

--- drivers/net/wireless/bcmdhd/dhd_linux.c
+++ drivers/net/wireless/bcmdhd/dhd_linux.c
@@ -42,6 +42,8 @@
 #include <linux/ethtool.h>
 #include <linux/fcntl.h>
 #include <linux/fs.h>
+#include <linux/ip.h>
+#include <net/addrconf.h>
 
 #include <asm/uaccess.h>
 #include <asm/unaligned.h>
